0944599bc44202b20ade691234de7198285f757c
Andrii Kononchuk <kononcukandrij@gmail.com>
2024-10-06 15:50:18 +0200

    active state for navigation buttons

---
+++ src/js/header.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname;
+    const homeLink = document.querySelector(".home");
+    const shoppingListLink = document.querySelector(".shopping-list");
+
+    if (currentPath.includes("index.html")) {
+        homeLink.classList.add("active");
+    } else if (currentPath.includes("shopping-list.html")) {
+        shoppingListLink.classList.add("active");
+    }
+});
