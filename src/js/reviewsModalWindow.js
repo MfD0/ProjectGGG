ccd28ed8db5355800912d060fd4f54360f043653
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2024-11-20 13:20:00 +0200

    Create reviews modal window (#34)
    * add reviewers images
    
    * create "reviewsModalWindow.html"
    
    * add styles for reviews window
    
    * create "reviewsModalWindow.js"
    
    * include created files in the page
    
    * fix reviewers images paths
    
    * fix selecting wrong modal window
    
    * revert formatting changes in index.html

---
+++ src/js/reviewsModalWindow.js
@@ -0,0 +1,29 @@
+window.openReviewsWindow = openReviewsWindow;
+
+// Шукаємо модальне вікно "About"
+const modal = document.querySelectorAll('.modal')[3];
+const backdrop = modal.querySelector('.modal-body');
+const closeModalButton = modal.querySelector('.modal-close');
+
+function openReviewsWindow() {
+  // Відображаємо вікно та блокуємо прокрутку фону
+  modal.classList.add('open');
+  document.body.style.overflow = 'hidden';
+}
+// Функція для закриття модального вікна
+function closeModal() {
+  modal.classList.remove('open');
+  document.body.style.overflow = ''; // Відновлюємо прокрутку фону
+}
+closeModalButton.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function (event) {
+  if (event.target === backdrop) {
+    closeModal();
+  }
+});
+// Закриваємо модальне вікно при натисканні на Escape
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
