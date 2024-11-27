0691af24a6a8e1089f3782c528587f5b65db2a20
Maxim <103531640+MfD0@users.noreply.github.com>
2024-11-27 17:17:18 +0200

    delete db files

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,43 +1,25 @@
-import { getUsers, addUser } from './api.js';
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
 
-console.log("auth.js");
+    loginForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
 
-// Авторизація
-document.getElementById("loginForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+        try {
+            const response = await fetch('../database/isers.json');
+            const users = await response.json();
 
-    console.log("username:", username);
-    console.log("password:", password);
+            const user = users.find(u => u.username === username && u.password === password);
 
-    try {
-        const users = await getUsers();
-        const user = users.find((u) => u.username === username && u.password === password);
-
-        if (user) {
-            localStorage.setItem("activeUser", user.id);
-            alert("Успішний вхід!");
-            window.location.href = "index.html";
-        } else {
-            alert("Невірний логін або пароль.");
+            if (user) {
+                sessionStorage.setItem('loggedInUser', JSON.stringify(user));
+                window.location.href = 'index.html';
+            } else {
+                alert('Невірний логін або пароль');
+            }
+        } catch (error) {
+            console.error('Помилка при авторизації:', error);
         }
-    } catch (error) {
-        console.error("Помилка авторизації:", error);
-    }
-});
-
-// Реєстрація
-document.getElementById("registerForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const username = document.getElementById("registerUsername").value;
-    const password = document.getElementById("registerPassword").value;
-
-    try {
-        await addUser({ username, password, cart: [] });
-        alert("Успішна реєстрація!");
-        window.location.href = "auth.html";
-    } catch (error) {
-        console.error("Помилка реєстрації:", error);
-    }
+    });
 });
