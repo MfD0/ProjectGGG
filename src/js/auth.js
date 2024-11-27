64ca06a734f95c4393a39215adca6ea83e4608f6
Maxim <103531640+MfD0@users.noreply.github.com>
2024-11-27 19:07:04 +0200

    додає перевірку на помилки

--- src/js/auth.js
+++ src/js/auth.js
@@ -7,13 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
+        // https://raw.githubusercontent.com/MfD0/ProjectGGG/refs/heads/main/src/database/users.json <= шлях для GitHub Pages
+        // ../database/users.json <= шлях для локального
+        const USERS_JSON_URL = 'https://raw.githubusercontent.com/MfD0/ProjectGGG/main/src/database/users.json';
+
         try {
             console.log("Запит на базу даних");
-            const response = await fetch('https://raw.githubusercontent.com/MfD0/ProjectGGG/refs/heads/main/src/database/users.json');
+            const response = await fetch(USERS_JSON_URL);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
             
             console.log("тіло try");
-            // https://raw.githubusercontent.com/MfD0/ProjectGGG/refs/heads/main/src/database/users.json <= шлях для GitHub Pages
-            // ../database/users.json <= шлях для локального
+            
             const users = await response.json();
 
             const user = users.find(u => u.username === username && u.password === password);
